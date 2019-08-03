82061efe1abe6488ec15c9bee96fbbd88dae7937
David <david.schwarze2001@gmail.com>
2019-08-03 13:18:58 +0200

    Added first service classes and interfaces without any methods.

---
+++ src/de/david/inez/services/CorrectionServiceImpl.java
@@ -0,0 +1,10 @@
+package de.david.inez.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CorrectionServiceImpl implements CorrectionService {
+
+	
+	
+}
---
+++ src/de/david/inez/services/UnitServiceImpl.java
@@ -0,0 +1,8 @@
+package de.david.inez.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UnitServiceImpl implements UnitService {
+
+}

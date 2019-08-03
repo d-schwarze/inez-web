40ab393322464d76069669b4b31eb541a09a92f3
David <david.schwarze2001@gmail.com>
2019-08-03 11:34:39 +0200

    Added the application properties file for apache derby database.

--- src/de/david/inez/Application.java
+++ src/de/david/inez/Application.java
@@ -2,10 +2,8 @@ package de.david.inez;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
